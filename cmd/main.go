0adfb2e894a02cd96c0d3463ec4a8f0d04ae2a24
imjowend <joaquinvcestudio@gmail.com>
2024-03-28 13:53:17 -0500

    Addition

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/imjowend/hex-arch-golang/internal/adapters/core/arithmetic"
+)
+
+func main() {
+	arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
 }
