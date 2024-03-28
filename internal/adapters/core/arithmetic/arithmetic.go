49001d3e34428a258553919895ade6223462da2a
imjowend <joaquinvcestudio@gmail.com>
2024-03-27 23:07:20 -0500

    adapter

---
+++ internal/adapters/core/arithmetic/arithmetic.go
@@ -0,0 +1,20 @@
+package arithmetic
+
+type Adapter struct {
+}
+
+func (arith Adapter) Addition(a int32, b int32) (int32, error) {
+	return a + b, nil
+}
+
+func (arith Adapter) Substraction(a int32, b int32) (int32, error) {
+	return a - b, nil
+}
+
+func (arith Adapter) Multiplication(a int32, b int32) (int32, error) {
+	return a * b, nil
+}
+
+func (arith Adapter) Division(a int32, b int32) (int32, error) {
+	return a / b, nil
+}
