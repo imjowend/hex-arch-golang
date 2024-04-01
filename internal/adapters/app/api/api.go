0771a286a898fe772e9df29f768acad53af42ed8
imjowend <joaquinvcestudio@gmail.com>
2024-03-31 22:53:10 -0500

    App layer

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -1,1 +1,47 @@
 package api
+
+import (
+	"github.com/imjowend/hex-arch-golang/internal/ports"
+)
+
+type Adapter struct {
+	arith ports.AritmethicPort
+}
+
+func NewAdapter(arith ports.AritmethicPort) *Adapter {
+	return &Adapter{
+		arith: arith,
+	}
+}
+
+func (apiAdapter Adapter) GetAddition(a, b int32) (int32, error) {
+	answer, err := apiAdapter.arith.Addition(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apiAdapter Adapter) GetSubstraction(a, b int32) (int32, error) {
+	answer, err := apiAdapter.arith.Substraction(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apiAdapter Adapter) GetMultiplication(a, b int32) (int32, error) {
+	answer, err := apiAdapter.arith.Multiplication(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
+
+func (apiAdapter Adapter) GetDivision(a, b int32) (int32, error) {
+	answer, err := apiAdapter.arith.Division(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return answer, nil
+}
